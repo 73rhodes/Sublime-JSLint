f9f0b1f7b0406927aee211f2f6b24461d7b1b64a
Darren DeRidder <drderidder@gmail.com>
2012-08-10 17:06:38 -0400

    Renaming

---
+++ JSLint.py
@@ -0,0 +1,17 @@
+import sublime, sublime_plugin, re
+
+class autoJSLint(sublime_plugin.EventListener):
+  def on_post_save(self, view):
+    settings = sublime.load_settings("JSLint.sublime-settings")
+    if re.search( settings.get( "filename_filter" ), view.file_name() ):
+      view.window().run_command('set_build_system', {
+        'file': 'Packages/JSLint/JSLint.sublime-build'
+      })
+      view.window().run_command( "build" )
+
+class JslintCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    self.window.run_command('set_build_system', {
+      'file': 'Packages/JSLint/JSLint.sublime-build'
+    })
+    self.window.run_command('build')

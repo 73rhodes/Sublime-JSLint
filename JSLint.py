23c95c84a7b5de23a131e53e10af923d0466375a
Darren DeRidder <drderidder@gmail.com>
2012-09-13 11:26:11 -0400

    Don't force the build system to switch to JSLint on save

--- JSLint.py
+++ JSLint.py
@@ -4,9 +4,6 @@ class autoJSLint(sublime_plugin.EventListener):
   def on_post_save(self, view):
     settings = sublime.load_settings("JSLint.sublime-settings")
     if re.search( settings.get( "filename_filter" ), view.file_name() ):
-      view.window().run_command('set_build_system', {
-        'file': 'Packages/JSLint/JSLint.sublime-build'
-      })
       view.window().run_command( "build" )
 
 class JslintCommand(sublime_plugin.WindowCommand):

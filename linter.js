1b94801e1462437033f083717cbae59aefcddb20
Darren DeRidder <darren.deridder@shopify.com>
2015-10-15 15:03:38 -0400

    Update to JSLint version 2015-10-08

--- linter.js
+++ linter.js
@@ -1,66 +1,67 @@
-var JSLINT = require('./jslint');
+var jslint = require('./jslint');
 var fs = require('fs');
 
 // Skip first two args (node & linter.js)
 var args = process.argv;
 args.splice(0, 2);
 
-var argsWithVals  = ["indent", "maxerr", "maxlen", "predef"];
+var argsWithVals = ["indent", "maxerr", "maxlen", "predef"];
 var valueExpected = null;
 var srcFile = null;
 var options = {};
 
-args.forEach(function (val, index, array) {
-  if (/^--[^\s]*$/.test(val)) {
-    var arg = val.replace(/^--/, '');
-    if (argsWithVals.indexOf(arg) >= 0) {
-      valueExpected = arg;
+args.forEach(function (val, index) {
+    if (/^--[^\s]*$/.test(val)) {
+        var arg = val.replace(/^--/, '');
+        if (argsWithVals.indexOf(arg) >= 0) {
+            valueExpected = arg;
+        } else {
+            //console.log('Setting ' + arg + '=true ')
+            valueExpected = null;
+            options[arg] = true;
+        }
     } else {
-      valueExpected = null;
-      options[arg] = true;
+        if (valueExpected) {
+            if (val.indexOf('[') === 0) {
+                val = eval(val);
+            }
+            //console.log('Setting ' + valueExpected + "=" + val);
+            options[valueExpected] = val;
+            valueExpected = null;
+        } else if (index === args.length - 1) {
+            srcFile = val;
+        }
     }
-  } else {
-    if (valueExpected) {
-      if (val.indexOf('[') === 0) {
-        val = eval(val);
-      }
-      options[valueExpected] = val;
-      valueExpected = null;
-    } else if (index === args.length - 1) {
-      srcFile = val;
-    }
-  }
 });
 
-var srcString = fs.readFile(srcFile, function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    data = data.toString('utf8');
-    data = data.replace(/^\#\!.*/, "");
-    var success = JSLINT(data, options),
-      len = JSLINT.errors.length,
-      fileMessage = "\n" + srcFile,
-      pad,
-      line,
-      e,
-      i;
-    if (options.json) {
-      console.log(JSON.stringify([srcFile, JSLINT.errors]));
+fs.readFile(srcFile, function (err, data) {
+    'use strict';
+    if (err) {
+        console.log(err);
     } else {
-      console.log(fileMessage);
-      for (i = 0; i < len; i += 1) {
-        pad = "#" + String(i + 1);
-        while (pad.length < 3) {
-          pad = ' ' + pad;
+        data = data.toString('utf8');
+        data = data.replace(/^\#\!.*/, "");
+        var result = jslint(data, options);
+        //var len = result.warnings.length;
+        var fileMessage = "\n" + srcFile;
+        var pad, line, e, sourceLine;
+        if (options.json) {
+            console.log(JSON.stringify([srcFile, result.warnings]));
+        } else {
+            console.log(fileMessage);
+            result.warnings.forEach(function (val, index) {
+                pad = "#" + String(index + 1);
+                while (pad.length < 3) {
+                    pad = ' ' + pad;
+                }
+                e = val;
+                if (e) {
+                    line = ' // Line ' + (e.line + 1) + ', Pos ' + (e.column + 1);
+                    console.log(pad + ' ' + e.message);
+                    sourceLine = result.lines[e.line] || '';
+                    console.log('    ' + sourceLine.replace(/^\s+|\s+$/, "") + line);
+                }
+            });
         }
-        e = JSLINT.errors[i];
-        if (e) {
-          line = ' // Line ' + e.line + ', Pos ' + e.character;
-          console.log(pad + ' ' + e.reason);
-          console.log('    ' + (e.evidence || '').replace(/^\s+|\s+$/, "") + line);
-        }
-      }
     }
-  }
 });
